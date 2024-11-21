ef10dbbb50277056e2d1061c34d44893e257f8b5
s230181 <gimpo.s230181@ggh.goe.go.kr>
2024-11-21 11:27:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ with col1:
 # 3.데이터시각화
 with col2:
       st.subheader('데이터시각화1')
-      st.image('____________' )   # 이미지 불러오기
+      st.image('나이에 따른 감정.PNG' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화2')
-      st.image('____________')    # 이미지 불러오기
+      st.image('사용시간에 따른 감정1.PNG')    # 이미지 불러오기
 
 # 4. 모델 활용
 st.subheader('모델 활용')
