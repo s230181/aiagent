1afc1a4374910973f5c807ed14a22bd518137a2e
s230181 <gimpo.s230181@ggh.goe.go.kr>
2024-11-25 14:24:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ with col3:
 st.subheader('모델 활용')
 st.write('**** 소셜미디어 하루 사용 시간이 얼마나 되나요? 인공지능이 당신의 기분을 예측해드립니다!!')
 
-a = st.number_input(' 나이를 입력하세요. ', value=0)
-b = st.number_input(' 소셜미디어 하루 사용 시간을 입력하세요. ', value=0)   # 사용자 입력
+b = st.number_input(' 나이를 입력하세요. ', value=0)
+a = st.number_input(' 소셜미디어 하루 사용 시간을 입력하세요. ', value=0)   # 사용자 입력
 
 if st.button('나의 감정 확인하기'):              # 사용자가 '합불분류' 버튼을 누르면
         input_data = [[ a,b ]]          # 사용자가 입력한 a,b,c 를 input_data에 저장하고
